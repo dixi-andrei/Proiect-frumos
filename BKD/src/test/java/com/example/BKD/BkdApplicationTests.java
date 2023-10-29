e5b6d83a6759ac6584191287a7ede3d2227fd738
dixi-andrei <andrei-adelin.ditoiu@s.unibuc.ro>
2023-10-30 00:17:23 +0200

    Am adaugat proiectul BKD

---
+++ BKD/src/test/java/com/example/BKD/BkdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BKD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BkdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
